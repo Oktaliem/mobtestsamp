6aa7742ac20887b7952ea5715777100ff675686b
Okta Liem <surya_diharja@yahoo.co.id>
2020-05-31 11:26:20 +0800

    video recording during test and attach to allure report

--- src/test/java/com/oktaliem/testsuit/Preparation.java
+++ src/test/java/com/oktaliem/testsuit/Preparation.java
@@ -8,13 +8,20 @@ import com.testinium.deviceinformation.exception.DeviceNotFoundException;
 import com.testinium.deviceinformation.model.Device;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.screenrecording.CanRecordScreen;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.apache.commons.codec.binary.Base64;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Preparation {
     AppiumDriver driver;
@@ -32,7 +39,9 @@ public class Preparation {
         capabilities.setCapability("app", System.getProperty("user.dir") + "/src/test/resources/eribank.apk");
         capabilities.setCapability("appPackage", "com.experitest.eribank");
         capabilities.setCapability("appActivity", "com.experitest.ExperiBank.LoginActivity");
+        capabilities.setCapability("automationName","UiAutomator2");
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        ((CanRecordScreen)driver).startRecordingScreen();
         user_is_on = new User(driver);
         user_is_on.loginPage().loginToApp();
         user_is_on.loginPage().landingToMenuPage();
@@ -40,9 +49,24 @@ public class Preparation {
 
 
     @AfterMethod
-    public void teardown() {
+    public void teardown() throws Exception {
+        String base64String = ((CanRecordScreen)driver).stopRecordingScreen();
+        byte[] data = Base64.decodeBase64(base64String);
+        String destinationPath="target/filename.mp4";
+        Path path = Paths.get(destinationPath);
+        Files.write(path, data);
+        attachVideo(destinationPath);
         driver.quit();
     }
 
+    @Attachment(value = "video",type="video/mp4")
+    public byte[] attachVideo(String path) throws Exception {
+        return getFile(path);
+    }
+
+    public byte[] getFile(String fileName) throws Exception {
+        File file = new File(fileName);
+        return Files.readAllBytes(Paths.get(file.getAbsolutePath()));
+    }
 
 }
